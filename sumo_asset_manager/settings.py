78b471ba9813665f3518a792565d33171ded07fc
rgsax <riccardogiordano1997@gmail.com>
2022-09-20 22:34:25 +0200

    security first

--- sumo_asset_manager/settings.py
+++ sumo_asset_manager/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+from dotenv import load_dotenv
+from os import getenv
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m0q@pih)&a-c4&g#bz=!nj5td6ztl70zd($13wpxpd5i#9ti6*'
+SECRET_KEY = getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
